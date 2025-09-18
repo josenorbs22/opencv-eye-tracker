55bef9045ea7e30815edacd1959662707b1fd986
José Norberto Fagundes Isaias <norbertojose007@gmail.com>
2025-09-18 14:42:44 -0300

    Algoritmo está gerando imagens. Adicionado suporte a CUDA e cuDNN

--- Eye-tracker/Eye-tracker.cpp
+++ Eye-tracker/Eye-tracker.cpp
@@ -13,7 +13,35 @@ void tracking() {
 	cv::Mat frame;
 	cv::VideoWriter writer("resultado.mp4",
 		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
-		30,
+		cap.get(cv::CAP_PROP_FPS),
+		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)),
+		false
+	);
+	cv::VideoWriter writer_rot("resultado_rot.mp4",
+		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
+		cap.get(cv::CAP_PROP_FPS),
+		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT))
+	);
+	cv::VideoWriter writer_crop("resultado_crop.mp4",
+		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
+		cap.get(cv::CAP_PROP_FPS),
+		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT))
+	);
+	cv::VideoWriter writer_gray("resultado_gray.mp4",
+		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
+		cap.get(cv::CAP_PROP_FPS),
+		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)),
+		false
+	);
+	cv::VideoWriter writer_blur("resultado_blur.mp4",
+		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
+		cap.get(cv::CAP_PROP_FPS),
+		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)),
+		false
+	);
+	cv::VideoWriter writer_threshold("resultado_threshold.mp4",
+		cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
+		cap.get(cv::CAP_PROP_FPS),
 		cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)),
 		false
 	);
@@ -56,10 +84,18 @@ void tracking() {
 		cv::Mat left_gray_crop;
 		cv::Mat right_gray_crop;
 
-		cv::Mat left_rot_mat = cv::getRotationMatrix2D(left_eye_rect.center, left_eye_rect.angle, 1.0);
-		cv::Mat right_rot_mat = cv::getRotationMatrix2D(right_eye_rect.center, right_eye_rect.angle, 1.0);
+		/*cv::Mat left_rot_mat = cv::getRotationMatrix2D(left_eye_rect.center, left_eye_rect.angle, 1.0);
+		cv::Mat right_rot_mat = cv::getRotationMatrix2D(right_eye_rect.center, right_eye_rect.angle, 1.0);*/
+		cv::Mat left_rot_mat = cv::getRotationMatrix2D(left_eye_rect.center, 0, 1.0);
+		cv::Mat right_rot_mat = cv::getRotationMatrix2D(right_eye_rect.center, 0, 1.0);
 		cv::warpAffine(frame, left_rotated, left_rot_mat, frame.size());
 		cv::warpAffine(frame, right_rotated, right_rot_mat, frame.size());
+
+		//Criar vídeo só com os olhos rotacionados
+		cv::Mat rot = left_rotated;
+		writer_rot.write(rot);
+		//-------------------------
+
 		cv::Size left_size(
 			std::abs(shapes.at(0).part(39).x() - shapes.at(0).part(36).x()),
 			std::abs(shapes.at(0).part(41).y() - shapes.at(0).part(38).y())
@@ -73,11 +109,29 @@ void tracking() {
 		cv::getRectSubPix(left_rotated, dimensions, left_center, left_bounding_crop);
 		cv::getRectSubPix(right_rotated, dimensions, right_center, right_bounding_crop);
 
+		//Criar vídeo só com os olhos cortados
+		cv::Mat crop = left_bounding_crop;
+		cv::resize(crop, crop, frame.size());
+		writer_crop.write(crop);
+		//-------------------------
+
 		cv::cvtColor(left_bounding_crop, left_gray_crop, cv::COLOR_BGR2GRAY);
 		cv::cvtColor(right_bounding_crop, right_gray_crop, cv::COLOR_BGR2GRAY);
 
+		//Criar vídeo cinza
+		cv::Mat gray = left_gray_crop;
+		cv::resize(gray, gray, frame.size());
+		writer_gray.write(gray);
+		//-------------------------
+
 		cv::GaussianBlur(left_gray_crop, left_gray_crop, cv::Size(5, 5), 0);
 		cv::GaussianBlur(right_gray_crop, right_gray_crop, cv::Size(5, 5), 0);
+
+		//Criar vídeo desfocado
+		cv::Mat blur = left_gray_crop;
+		cv::resize(blur, blur, frame.size());
+		writer_blur.write(blur);
+		//-------------------------
 
 		for (int y = 0; y < left_gray_crop.rows; y++) {
 			for (int x = 0; x < left_gray_crop.cols; x++) {
@@ -114,8 +168,14 @@ void tracking() {
 		/*cv::threshold(left_gray_crop, left_bin_crop, left_bin_slider, 255, cv::THRESH_BINARY_INV);
 		cv::threshold(right_gray_crop, right_bin_crop, right_bin_slider, 255, cv::THRESH_BINARY_INV);*/
 
-		cv::threshold(left_gray_crop, left_bin_crop, 127, 255, cv::THRESH_BINARY_INV);
-		cv::threshold(right_gray_crop, right_bin_crop, 127, 255, cv::THRESH_BINARY_INV);
+		cv::threshold(left_gray_crop, left_bin_crop, 50, 255, cv::THRESH_BINARY_INV);
+		cv::threshold(right_gray_crop, right_bin_crop, 50, 255, cv::THRESH_BINARY_INV);
+
+		//Criar vídeo apenas com pontos brancos e pretos
+		cv::Mat threshold = left_bin_crop;
+		cv::resize(threshold, threshold, frame.size());
+		writer_threshold.write(threshold);
+		//-------------------------
 
 		cv::Mat bin_sum;
 		cv::bitwise_and(left_bin_crop, right_bin_crop, bin_sum);
@@ -144,11 +204,18 @@ void tracking() {
 		cv::dilate(bin_sum, bin_sum, erode_mat, cv::Point(-1, -1));
 
 		
-		cv::cvtColor(bin_sum, bin_sum, cv::COLOR_GRAY2BGR);
-		bin_sum.copySize(frame);
+		//cv::cvtColor(bin_sum, bin_sum, cv::COLOR_GRAY2BGR);
+		//bin_sum.copySize(frame);
+		cv::resize(bin_sum, bin_sum, frame.size());
+
 		writer.write(bin_sum);
 	}
 	writer.release();
+	writer_rot.release();
+	writer_crop.release();
+	writer_gray.release();
+	writer_blur.release();
+	writer_threshold.release();
 	cap.release();
 }
 
